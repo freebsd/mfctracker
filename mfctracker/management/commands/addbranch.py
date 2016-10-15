3b78ef24f1ea92c73a13359ac077d79e3d9d9c16
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-10-15 16:28:48 -0700

    Add setting branchpoint for new branch
    It can be provided either using -b command-line switch or by
    automatically detecting it using svn log command

--- mfctracker/management/commands/addbranch.py
+++ mfctracker/management/commands/addbranch.py
@@ -1,4 +1,7 @@
+import svn.remote
+
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 from mfctracker.models import Branch
 
 class Command(BaseCommand):
@@ -7,15 +10,29 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-n', '--name', required=True, help='branch name')
         parser.add_argument('-p', '--path', required=True, help='branch path')
+        parser.add_argument('-b', '--branch-point', type=int, default=None, help='branch point revision')
 
     def handle(self, *args, **options):
+        branchpoint = options['branch_point']
         name = options['name']
         path = options['path'].strip('/')
         path = '/' + path
+        if not branchpoint:
+            try:
+                r = svn.remote.RemoteClient(settings.SVN_BASE_URL)
+                entries = list(r.log_default(rel_filepath=path, limit=1, revision_from=0, revision_to='HEAD', stop_on_copy=True))
+                if len(entries) != 1:
+                    raise ValueError('Invalid number of entries ({}) returned for branch point query'.format(len(entries)))
+                branchpoint = entries[0].revision
+            except Exception as e:
+                self.stdout.write(self.style.ERROR('Failed to get branch point for branch: %s' % e.message))
+
         branch = Branch.create(name, path)
+        branch.branch_revision = branchpoint
+        branch.last_revision = branchpoint - 1
+
         try:
             branch.save()
-            self.stdout.write(self.style.SUCCESS('Branch %s created with path %s' %  (name, path)))
+            self.stdout.write(self.style.SUCCESS('Branch {} created with path {}, branch point: {}'.format(name, path, branchpoint)))
         except Exception as e:
             self.stdout.write(self.style.ERROR('Error adding new branch: %s' % e.message))
-
