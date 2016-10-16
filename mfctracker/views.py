dc46dd35e3be6cfd429b3d810a7ab3d130a88897
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-10-15 19:13:05 -0700

    Add branch selection

--- mfctracker/views.py
+++ mfctracker/views.py
@@ -2,10 +2,18 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect, get_object_or_404
 
 from .models import Branch, Commit
 
 def index(request):
+    branches = Branch.objects.filter(~Q(name='HEAD')).order_by('-branch_revision', '-name')
+    default_pk = branches[0].pk
+    return redirect('branch', branch_id=default_pk)
+
+def branch(request, branch_id):
+    current_branch = get_object_or_404(Branch, pk=branch_id)
+
     template = loader.get_template('mfctracker/index.html')
     head = Branch.head()
     branches = Branch.objects.filter(~Q(name='HEAD')).order_by('-branch_revision', '-name')
@@ -27,5 +35,5 @@ def index(request):
         # If page is out of range (e.g. 9999), deliver last page of results.
         commits = paginator.page(paginator.num_pages)
 
-    context = {'commits': commits, 'branches': branches, 'current_branch': head }
+    context = {'commits': commits, 'branches': branches, 'current_branch': current_branch }
     return HttpResponse(template.render(context, request))
