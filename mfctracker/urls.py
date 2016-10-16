e09b55077b9ffcbab6c541f7e1a984860c93ffd0
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-10-15 23:51:48 -0700

    Add filter by MFC status

--- mfctracker/urls.py
+++ mfctracker/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<branch_id>[0-9]+)/$', views.branch, name='branch'),
+    url(r'^(?P<branch_id>[0-9]+)/setfilter$', views.setfilter, name='setfilter'),
 ]
--- mfctracker/views.py
+++ mfctracker/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from django.http import HttpResponse
@@ -13,6 +15,16 @@ def index(request):
         default_pk = branches[0].pk
     return redirect('branch', branch_id=default_pk)
 
+def setfilter(request, branch_id):
+    author = request.GET.get('author', None)
+    filter_waiting = request.GET.get('filter_waiting', None)
+    filter_ready = request.GET.get('filter_ready', None)
+    request.session['author'] = author
+    request.session['filter_waiting'] = filter_waiting is not None
+    request.session['filter_ready'] = filter_ready is not None
+
+    return redirect('branch', branch_id=branch_id)
+
 def branch(request, branch_id):
     current_branch = get_object_or_404(Branch, pk=branch_id)
     request.session['branch'] = branch_id
@@ -22,34 +34,52 @@ def branch(request, branch_id):
     branches = Branch.objects.filter(~Q(name='HEAD')).order_by('-branch_revision', '-name')
     query = head.commit_set.filter(revision__gt=current_branch.branch_revision)
 
-    author = request.GET.get('author', None)
-    if author is None:
-        author = request.session.get('author', None)
-    else:
-        request.session['author'] = author
+    author = request.session.get('author', None)
+    filter_waiting = request.session.get('filter_waiting', False)
+    filter_ready = request.session.get('filter_ready', False)
 
     if author:
         query = query.filter(author=author)
     else:
         author = ''
 
+    q = Q()
+    if filter_ready:
+        q  = q | Q(mfc_after__lte=date.today())
+    if filter_waiting:
+        q = q | Q(mfc_after__gt=date.today())
+
+    query = query.filter(q)
+
     all_commits = query.order_by('-revision')
     paginator = Paginator(all_commits, 15)
 
     page = request.GET.get('page')
+    if page is None:
+        page = request.session.get('page', None)
+
     try:
         commits = paginator.page(page)
+        request.session['page'] = page
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
         commits = paginator.page(1)
+        request.session['page'] = 1
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         commits = paginator.page(paginator.num_pages)
+        request.session['page'] = paginator.num_pages
 
     context = {}
     context['commits'] = commits
     context['branches'] = branches
     context['current_branch'] = current_branch
     context['author'] = author
+
+    if filter_waiting:
+        context['waiting_checked'] = 'checked'
+
+    if filter_ready:
+        context['ready_checked'] = 'checked'
 
     return HttpResponse(template.render(context, request))
