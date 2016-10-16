32fc8dd4426fbb64dd9d21330619b7c441c04f82
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-10-16 15:54:03 -0700

    Add missing pieces to production settings

--- mfctracker/settings/production.py
+++ mfctracker/settings/production.py
@@ -2,10 +2,13 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['*']
+
 SECRET_KEY = open(os.path.expanduser('~/.mfctracker-django-secret')).read().strip()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mfctracker',
     }
 }
