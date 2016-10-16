dce38ff9f33f9efe42dfcc371d04c48f1c13caf8
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-10-15 17:26:57 -0700

    Add bootstrap and navigation for revisions

--- mfctracker/settings.py
+++ mfctracker/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mfctracker'
+    'bootstrap3',
+    'mfctracker',
 ]
 
 MIDDLEWARE = [
--- mfctracker/views.py
+++ mfctracker/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.template import loader
 
@@ -6,6 +7,18 @@ from .models import Branch, Commit
 def index(request):
     template = loader.get_template('mfctracker/index.html')
     head = Branch.head()
-    commits = head.commit_set.order_by('-revision')[:10]
-    context = {'commits': commits}
+    all_commits = head.commit_set.order_by('-revision')
+    paginator = Paginator(all_commits, 15)
+
+    page = request.GET.get('page')
+    try:
+        commits = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        commits = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        commits = paginator.page(paginator.num_pages)
+
+    context = {'commits': commits }
     return HttpResponse(template.render(context, request))
