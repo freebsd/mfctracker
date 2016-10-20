9daba3dacc6dfbf94e90962de6cc5253882aceef
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2016-10-20 15:45:09 -0700

    Move development to PgSQL and add django-debug-toolbar

--- mfctracker/settings/development.py
+++ mfctracker/settings/development.py
@@ -1,17 +1,22 @@
 from .base import *
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'o4c6#^(x44smd70-4=yghiguu25m^89bmtsufxqpn-!e(gqoy8'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mfctracker_dev',
     }
+}
+
+INSTALLED_APPS += [ 'debug_toolbar' ]
+MIDDLEWARE = [ 'debug_toolbar.middleware.DebugToolbarMiddleware' ] + MIDDLEWARE
+
+def custom_show_toolbar(request):
+    return True  # Always show toolbar
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'mfctracker.settings.custom_show_toolbar',
 }
--- mfctracker/urls.py
+++ mfctracker/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.conf import settings
 
 from . import views
 
@@ -13,3 +14,9 @@ urlpatterns = [
     url(r'^mfcbasket/clear$', views.clearbasket, name='clearbasket'),
     url(r'^commit/(?P<revision>[0-9]+)$', views.commit, name='commit'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
