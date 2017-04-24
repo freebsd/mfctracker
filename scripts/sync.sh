d6c59c55219a849331915ee63e2139ac9c39477d
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2017-04-24 13:36:48 -0700

    Add script to synchronize commits

---
+++ scripts/sync.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+lockfile=/var/tmp/mfctracker.sync.lock
+
+if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null; then
+
+    trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+    /usr/local/bin/mfctracker-manage importcommits -l 1000
+
+    # clean up after yourself, and release your trap
+    rm -f "$lockfile"
+    trap - INT TERM EXIT
+else
+        echo "Lock exists: $lockfile owned by $(cat $lockfile)"
+fi
