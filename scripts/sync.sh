980a4e3ef766c8f380ebca55c2147820851efa65
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2017-04-25 16:04:03 -0700

    Make sync.sh pass-through command line arguments to importcommits

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,7 +5,7 @@ lockfile=/var/tmp/mfctracker.sync.lock
 if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null; then
 
     trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
-    /usr/local/bin/mfctracker-manage importcommits -l 1000
+    /usr/local/bin/mfctracker-manage importcommits $*
 
     # clean up after yourself, and release your trap
     rm -f "$lockfile"
